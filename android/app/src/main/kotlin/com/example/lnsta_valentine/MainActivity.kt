b11eed236851dab1f1ac6ba5872b1b512ae0f8b1
OnNa05 <Apirat.t@hotmail.com>
2022-02-27 00:00:55 +0700

    Can't align posts to the left

---
+++ android/app/src/main/kotlin/com/example/lnsta_valentine/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lnsta_valentine
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/CommentModel.dart
@@ -0,0 +1,31 @@
+class Comment {
+  String authorName;
+  String text;
+
+  Comment({
+    required this.authorName,
+    required this.text,
+  });
+}
+
+final List<Comment> commentsPost_1 = [
+  Comment(
+    authorName: 'Anathan OG',
+    text: 'อะไรนะ!!ฉันเพิ่งซื้อราคาเต็ม',
+  ),
+  Comment(
+    authorName: 'Joan deedee',
+    text: 'ไม่สนหรอก ฉันยืมเล่นจากคลังเพื่อน',
+  ),
+];
+
+final List<Comment> commentsPost_2 = [
+  Comment(
+    authorName: 'Bomber',
+    text: 'หืมมมม...จัดทีมยากแฮะ',
+  ),
+  Comment(
+    authorName: 'Regiregi',
+    text: 'ตระกูลRegiก็โกงอยู่นะ',
+  ),
+];
